dd2f017959e58eb700e8ddb2bb5f7661ba8f166e
GitHub Action <action@github.com>
2022-12-02 18:23:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/steveireland/generatepcmlbean/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/steveireland/generatepcmlbean/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.steveireland.generatepcmlbean.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.steveireland.generatepcmlbean.MyBundle
 
 class MyApplicationService {
 
